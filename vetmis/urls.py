6d45db1b728cfa66c252393dcae5e4ac4b996c1b
user <kimdanil025@gmail.com>
2025-12-26 17:33:54 +0300

    Обновление: ошибки, UI, публичные клиники

--- vetmis/urls.py
+++ vetmis/urls.py
@@ -18,5 +18,10 @@ urlpatterns = [
     path('services/', include('services.urls')),
 ]
 
+handler400 = 'django.views.defaults.bad_request'
+handler403 = 'django.views.defaults.permission_denied'
+handler404 = 'django.views.defaults.page_not_found'
+handler500 = 'django.views.defaults.server_error'
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
