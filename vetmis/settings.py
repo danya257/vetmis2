c2ed3896c0babcfa70951bbcfa6d880548a981a9
user <kimdanil025@gmail.com>
2026-01-23 09:31:22 +0300

    Обновление функционала / исправления

--- vetmis/settings.py
+++ vetmis/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'vetmis2.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
